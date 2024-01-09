4f3fd2d2e3e8f3250bbc45d14f23480a4d63d0e1
sirlearning <zhengnda@outlook.com>
2024-01-09 14:41:16 +0800

    2024.1.9

--- teLoad/count.py
+++ teLoad/count.py
@@ -4,13 +4,15 @@ import matplotlib.pyplot as plt
 categories = ['DHH', 'DTA', 'DTC', 'DTH', 'DTM', 'DTT', 'DTX', 'DXX', 'RIX', 'RLC', 'RLG', 'RLX', 'XXX']
 counts = [735, 1194, 1095976, 48828, 95785, 184898, 108199, 12509, 109873, 596077, 1586131, 147763, 285813]
 
-# 创建条形图
-plt.bar(categories, counts, color='blue')
+# 计算比例
+total_counts = sum(counts)
+percentages = [count / total_counts * 100 for count in counts]
 
-# 添加标题和标签
-plt.title('Category Counts')
-plt.xlabel('Categories')
-plt.ylabel('Counts')
+# 创建饼图
+plt.pie(percentages, labels=categories, autopct='%1.1f%%', startangle=140)
+
+# 添加标题
+plt.title('Category Percentages')
 
 # 显示图表
 plt.show()
