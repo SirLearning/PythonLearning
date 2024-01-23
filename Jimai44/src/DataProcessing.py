2ae45718755093abf23ab404b1eb4cd5e283656a
Zheng_Da <74081090+SirLearning@users.noreply.github.com>
2024-01-23 12:08:31 +0800

    2024.1.23

--- Jimai44/src/DataProcessing.py
+++ Jimai44/src/DataProcessing.py
@@ -13,3 +13,5 @@ def checkSeqLength(fileName, length):
 # checkSeqLength(fileName, 3)
 data = pd.read_csv("new.txt", sep = '\t')
 length_counts = data.groupby("length").size().reset_pwdindex(name='count')
+print(length_counts)
+length_counts.to_csv("length_counts.txt", sep = '\t', index = False)
